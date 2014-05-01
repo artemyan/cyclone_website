af55e695f9f7783d126c96ab9a65862debfb38a7
justvitalius <justvitalius@gmail.com>
2014-05-01 15:35:39 +0400

    Add empty app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cyclone::Application.config.secret_key_base = '90e1dbeed81b85a79bc43f8fead29fa95463683576e4471620cd4bdcd5efd32aa323bca0c940330208a0564affc2397ae4343d3e374768de2ca0d4eac3acf412'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cyclone::Application.config.session_store :cookie_store, key: '_cyclone_session'
