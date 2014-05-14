9fbfcc8c3456bc9e631c4d491f0e5673a764cace
justvitalius <justvitalius@gmail.com>
2014-05-14 13:02:30 +0400

    Add licenses content.

--- app/models/license.rb
+++ app/models/license.rb
@@ -6,13 +6,13 @@ class License
     @licenses = license
     @title = license[:title].html_safe
     @description = license[:description].html_safe
-    @pdf_url = generate_file_url(license[:file_name])
+    @file_url = generate_file_url(license[:file_name])
     @preview_url = generate_preview_url(license[:preview_name])
   end
 
 
   def generate_file_url(name)
-    "/system/licenses/files/#{name}" if name
+    "/system/licenses/#{name}" if name
   end
 
   def generate_preview_url(name)
