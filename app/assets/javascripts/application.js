59edeb28e3c51e8edc8cec1d056f768bc04b2d3c
justvitalius <justvitalius@gmail.com>
2014-05-16 16:43:29 +0400

    Add map to contacts.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,37 @@
 //= require jquery_ujs
 // require turbolinks
 //= require raphael
+//= require underscore
+//= require gmaps/google
 //= require_tree .
+
+
+
+handler = Gmaps.build('Google');
+handler.buildMap({
+                    provider: {
+                        disableDefaultUI: false,
+                        disableDoubleClickZoom: true,
+                        panControl: false,
+                        panControlOptions: false,
+                        rotateControl: false
+                    },
+                    internal: {id: 'map'}
+                },
+        function () {
+                    markers = handler.addMarkers([
+                                    {
+                                        "lat": 59.965894,
+                                        "lng": 30.292593,
+                                        "picture": {
+                                            "url": "/assets/map-marker.png",
+                                            "width": 32,
+                                            "height": 40
+                                        }
+                                    }
+                                ]);
+                    handler.bounds.extendWith(markers);
+                    handler.fitMapToBounds();
+                    handler.getMap().setZoom(17);
+        }
+);
